65fbcf01bd66ec0b05334172615a4599e505f6e9
Zhu Zhong <zhongzhu@ymail.com>
2016-03-22 23:15:54 +0800

    add object layer

--- js/load.js
+++ js/load.js
@@ -10,9 +10,7 @@ MyGame.Load.prototype = {
 
     this.load.tilemap('tilemap', 'assets/level.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('tiles', 'assets/spritesheet.png');
-    this.load.image('player', 'assets/alienBlue_stand.png');
-    // this.load.image('box', 'assets/boxCrate_double.png');
-    // this.load.image('water', 'assets/waterTop_low.png');
+    this.load.atlas('texture-atlas', 'assets/texture-atlas.png', 'assets/texture-atlas.json');
   },
 
   create: function () {
--- js/play.js
+++ js/play.js
@@ -12,8 +12,7 @@ MyGame.Play.prototype = {
     this.stage.backgroundColor = '#A6E5F5';
 
     this.map = this.add.tilemap('tilemap');    
-    this.map.addTilesetImage('myspritesheet', 'tiles');    
-    // this.map.addTilesetImage('background', 'BackgroundLayer');
+    this.map.addTilesetImage('myspritesheet', 'tiles');
 
     this.groundLayer = this.map.createLayer('GroundLayer');
     this.map.setCollisionBetween(1, 35, true, 'GroundLayer');
@@ -25,17 +24,21 @@ MyGame.Play.prototype = {
     console.log('game.width ' + this.game.width + " height " + this.game.height);
     console.log('world.width ' + this.world.width + " height " + this.world.height);  
 
-    this.player = this.add.sprite(60, 60, 'player');
-    this.player.anchor.setTo(0.5, 0.5);  
+    // initiate the player
+    this.player = this.add.sprite(60, 60, 'texture-atlas', 'alienBlue_stand');
+    this.player.anchor.setTo(0.5, 0.5);
+    this.player.animations.add('walking', Phaser.Animation.generateFrameNames('alienBlue_walk', 1, 2), 5, true);
+    this.player.animations.add('jumping', ['alienBlue_jump'], 1, false);    
+
     this.physics.arcade.enable(this.player);
-    this.player.body.gravity.y = 730;
+    this.player.body.gravity.y = 700;
 
     this.camera.follow(this.player);
 
     //  Here we create our enemies, put them in a group
-    // this.enemyGroup = this.game.add.group();
-    // this.enemyGroup.enableBody = true;
-    // this.map.createFromObjects('ObjectLayer', 'box', 'box', 0, true, false, this.enemyGroup);
+    this.enemyGroup = this.game.add.group();
+    this.enemyGroup.enableBody = true;
+    this.map.createFromObjects('ObjectLayer', 'blockerMad', 'texture-atlas', 'blockerMad', true, false, this.enemyGroup);
     // this.map.createFromObjects('ObjectLayer', 'water', 'water', 0, true, false, this.enemyGroup);    
 
     this.input.onDown.add(this.letsJump, this);
@@ -43,16 +46,18 @@ MyGame.Play.prototype = {
 
   letsJump: function() {
     if (this.player.body.blocked.down) {
-      this.player.body.velocity.y = -300;
+      this.player.body.velocity.y = -480;
+      this.player.animations.play('jumping');
     }
   },
   
   update: function() {
     this.physics.arcade.collide(this.player, this.groundLayer);
-    // this.physics.arcade.overlap(this.player, this.enemyGroup, this.playerHit, null, this);
+    this.physics.arcade.overlap(this.player, this.enemyGroup, this.playerHit, null, this);
      
     if (this.player.body.blocked.down) { 
       this.player.body.velocity.x = 170;
+      this.player.animations.play('walking');
     }
 
   },
