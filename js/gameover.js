fba76a2aadf5aa162f7944ff4216ef009be5d9cf
Zhu Zhong <zhongzhu@ymail.com>
2016-03-30 23:17:03 +0800

    working on game over state

--- js/gameover.js
+++ js/gameover.js
@@ -7,15 +7,25 @@ MyGame.GameOver.prototype = {
   },
 
   create: function() {
-    this.gameOverLabel = this.add.text(Math.round(this.game.width/2), 50, "Game Over", {font: "60px Arial", fill: "#fff", align: "center"}); 
+    this.gameOverLabel = this.add.text(Math.round(this.game.width/2), 100, "好可惜，20秒内没跑完100米", {font: "50px Arial", fill: "#fff", align: "center"}); 
     this.gameOverLabel.anchor.set(0.5);  	
 
-    this.playAgain = this.add.button(Math.round(this.game.width/2), 100, 'texture-atlas', this.onClick, this, 'alienBlue_front');
+    this.restartBtn = this.add.button(Math.round(this.game.width/2), Math.round(this.game.height/2), 'texture-atlas', this.onClick, this, 'restart', 'restart', 'restart', 'restart');
+    this.restartBtn.anchor.set(0.5);
+
+    this.menuBtn = this.add.button(Math.round(this.game.width/2), Math.round(this.game.height/2) + 100, 'texture-atlas', this.onClick, this, 'mainMenuBtn', 'mainMenuBtn', 'mainMenuBtn', 'mainMenuBtn');
+    this.menuBtn.anchor.set(0.5);    
   },
 
   onClick: function() {
   	console.log('you clicked');
+    this.state.start('Play');
   },
+
+  onMainMenu: function() {
+    console.log('go back to main menu');
+  },
+
   update: function() {}
 
 };
